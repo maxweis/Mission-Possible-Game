afea1b691ff2848fabb738b8cbfa310a5abdafd0
Max Weis <maxrobweis@gmail.com>
2015-06-27 01:27:45 -0500

    fix brace

--- src/event.c
+++ src/event.c
@@ -14,7 +14,8 @@ void InitEvent()
         key_hold = false;
 }
 
-void HandleButtonPress(){
+void HandleButtonPress()
+{
         SDL_EventState(SDL_MOUSEMOTION, SDL_IGNORE);
         SDL_PollEvent(&event);
         keystate = SDL_GetKeyboardState(NULL);
