63cbea0bbfb1b6bec5afac8cae787b5ba79dab06
Maxon Weis <maxrobweis@gmail.com>
2015-06-29 19:21:50 -0500

    add border collision detection

--- editor/src/collision.h
+++ editor/src/collision.h
@@ -1,5 +1,6 @@
 #include "defs.h"
 
 Direction Collision(SDL_Rect *a, SDL_Rect *b);
-Direction ScreenCollision(SDL_Rect *a);
+Direction ScreenCollision(SDL_Rect *a, int border);
+Direction BorderCollision(SDL_Rect *a, SDL_Rect *b, int border);
 void UpdateBound(Sprite *sprite);
